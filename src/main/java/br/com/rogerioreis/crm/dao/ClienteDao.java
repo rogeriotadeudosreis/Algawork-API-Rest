bc8b61fad2c3350940a56a2b7535ac7963e3250a
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-03-02 23:01:43 -0300

    Inicializando este repositorio com git

---
+++ src/main/java/br/com/rogerioreis/crm/dao/ClienteDao.java
@@ -0,0 +1,12 @@
+package br.com.rogerioreis.crm.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rogerioreis.crm.entity.Cliente;
+
+@Repository
+public interface ClienteDao extends JpaRepository<Cliente, Long> {
+	
+
+}
---
+++ src/main/java/br/com/rogerioreis/crm/entity/Cliente.java
@@ -0,0 +1,49 @@
+package br.com.rogerioreis.crm.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tb_cliente")
+public class Cliente implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	private int codigo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+}
